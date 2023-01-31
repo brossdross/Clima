7009c004bc5627c3211719b7a970fc495ff76c7a
Maximiliano <djmaxirey@gmail.com>
2023-01-31 05:50:52 -0300

    JS

--- main.js
+++ main.js
@@ -3,16 +3,30 @@ const api = {
     url: "http://api.openweathermap.org/data/2.5/weather"
 }
 
-
+const city = document.getElementById("city");
+const date = document.getElementById("date");
+const tempImg = document.getElementById("tempImg");
+const temp = document.getElementById("temp");
+const weather = document.getElementById("weather");
+const range = document.getElementById("range");
 
 async function search(query)
 try{
 const response = await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
 const data = await response.json();
-console.log(data);
+
+city.innerHTML = "${data.name}, ${data.sys.country}";
+data.innerHTML = (new date()).toLocaleDateString();
+temp.innerHTML = toCelsius(data.main.temp);
+weather.innerHTML = data.weather[0].description;
+range.innerHTML = "${toCelsius (data.main.temp_min)} / ${toCelsius (data.main.temp_max)}"
 } catch(err) {
     console.log(err);
     alert("hubo un error")
+}
+
+function toCelsius(kelvin) {
+    return Math.round(kelvin - 273.15);
 }
 
 function onsubmit(event) {
