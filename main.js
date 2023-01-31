d7b551cfea7b26c5a6ecd97d6924c4ba74297b10
Maximiliano <djmaxirey@gmail.com>
2023-01-31 06:38:50 -0300

    Detalles finales

--- main.js
+++ main.js
@@ -1,39 +1,54 @@
 const api = {
-    key: f03e41221ab16425119294545ea1c445,
+    key: "f03e41221ab16425119294545ea1c445",
     url: "http://api.openweathermap.org/data/2.5/weather"
 }
 
-const city = document.getElementById("city");
-const date = document.getElementById("date");
-const tempImg = document.getElementById("tempImg");
-const temp = document.getElementById("temp");
-const weather = document.getElementById("weather");
-const range = document.getElementById("range");
+const card = document.getElementById('card')
 
-async function search(query)
-try{
-const response = await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
-const data = await response.json();
+const city = document.getElementById('city');
+const date = document.getElementById('date');
+const tempImg = document.getElementById('temp-img');
+const temp = document.getElementById('temp');
+const weather = document.getElementById('weather');
+const range = document.getElementById('range');
 
-city.innerHTML = "${data.name}, ${data.sys.country}";
-data.innerHTML = (new date()).toLocaleDateString();
-temp.innerHTML = toCelsius(data.main.temp);
-weather.innerHTML = data.weather[0].description;
-range.innerHTML = "${toCelsius (data.main.temp_min)} / ${toCelsius (data.main.temp_max)}"
-} catch(err) {
+function updateImages(data) {
+  const temp = toCelsius(data.main.temp);
+  let src = 'images/temp-mid.png';
+  if (temp > 26) {
+    src = 'images/temp-high.png';
+  } else if (temp < 20) {
+    src = 'images/temp-low.png';
+  }
+  tempImg.src = src;
+}
+
+async function search(query) {
+  try {
+    const response = await fetch(`${api.url}?q=${query}&appid=${api.key}&lang=es`);
+    const data = await response.json();
+    card.style.display = 'block';
+    city.innerHTML = `${data.name}, ${data.sys.country}`;
+    data.innerHTML = (new Date()).toLocaleDateString();
+    temp.innerHTML = `${toCelsius(data.main.temp)}c`;
+    weather.innerHTML = data.weather[0].description;
+    range.innerHTML = `${toCelsius(data.main.temp_min)}c / ${toCelsius(data.main.temp_max)}c`;
+    updateImages(data);
+  } catch (err) {
     console.log(err);
-    alert("hubo un error")
+    alert('Hubo un error');
+  }
 }
 
 function toCelsius(kelvin) {
-    return Math.round(kelvin - 273.15);
+  return Math.round(kelvin - 273.15);
 }
 
-function onsubmit(event) {
-    event.preventDefault();
-    search(searchbox.value);
+function onSubmit(event) {
+  event.preventDefault();
+  search(searchbox.value);
 }
 
-const searchform = document.getElementById("search-form");
-const searchbox = document.getElementById("searchbox");
-searchform.addEventListener("submit" , onsubmit, true);
+const searchform = document.getElementById('search-form');
+const searchbox = document.getElementById('searchbox');
+searchform.addEventListener('submit', onSubmit, true);
